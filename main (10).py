fda3b54c7406f916b876ca58d46a183d36efd23f
do05267794-ship-it <do05267794@priestley.ac.uk>
2025-11-20 13:52:46 +0000

    Update main (10).py

--- main (10).py
+++ main (10).py
@@ -3,7 +3,7 @@ items = [23, 41, 11, 17, 34, 56]
 n=len(items) - 1
 
 swapped = True
-while swapped:
+while swapped and n > 0:
     swapped = False
     for index in range(0, n):
         if items[index] > items[index+1]:
